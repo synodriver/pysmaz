351e52aac0043694a3817d70112026d751d79bb6
synodriver <diguohuangjiajinweijun@gmail.com>
2022-07-06 15:12:43 +0000

    release gil

--- pysmaz/backends/cython/_smaz_cy.pyx
+++ pysmaz/backends/cython/_smaz_cy.pyx
@@ -13,8 +13,10 @@ cpdef inline bytes compress(const uint8_t[::1] data, Py_ssize_t output_size = 0)
     cdef bytes output = PyBytes_FromStringAndSize(NULL, output_size)
     if <void*>output == NULL:
         raise MemoryError
-
-    cdef int buffer_updated = smaz_compress(<char*>&data[0], <int>input_len, PyBytes_AS_STRING(output), <int>output_size)
+    cdef int buffer_updated
+    cdef char *output_ptr = PyBytes_AS_STRING(output)
+    with nogil:
+        buffer_updated = smaz_compress(<char*>&data[0], <int>input_len, output_ptr, <int>output_size)
     if buffer_updated == <int>input_len+1:
         raise ValueError("output buffer is too small")
     
@@ -28,8 +30,10 @@ cpdef inline bytes decompress(const uint8_t[::1] data, Py_ssize_t output_size = 
     cdef bytes output = PyBytes_FromStringAndSize(NULL, output_size)
     if <void*>output == NULL:
         raise MemoryError
-
-    cdef int buffer_updated = smaz_decompress(<char*>&data[0], <int>input_len, PyBytes_AS_STRING(output), <int>output_size)
+    cdef int buffer_updated
+    cdef char *output_ptr = PyBytes_AS_STRING(output)
+    with nogil:
+        buffer_updated = smaz_decompress(<char*>&data[0], <int>input_len, output_ptr, <int>output_size)
     if buffer_updated == <int>output_size+1:
         raise ValueError("output buffer is too small")
 
@@ -38,14 +42,18 @@ cpdef inline bytes decompress(const uint8_t[::1] data, Py_ssize_t output_size = 
 
 cpdef inline int compress_into(const uint8_t[::1] data, uint8_t[::1] output) except *:
     cdef Py_ssize_t output_size = output.shape[0]
-    cdef int ret =  smaz_compress(<char *> &data[0], <int> data.shape[0], <char*>&output[0], <int>output_size)
+    cdef int ret
+    with nogil:
+        ret =  smaz_compress(<char *> &data[0], <int> data.shape[0], <char*>&output[0], <int>output_size)
     if ret == <int>output_size + 1:
         raise ValueError("output buffer is too small")
     return ret
 
 cpdef inline int decompress_into(const uint8_t[::1] data, uint8_t[::1] output) except *:
     cdef Py_ssize_t output_size = output.shape[0]
-    cdef int ret =  smaz_decompress(<char *> &data[0], <int> data.shape[0], <char*>&output[0], <int>output_size)
+    cdef int ret
+    with nogil:
+        ret =  smaz_decompress(<char *> &data[0], <int> data.shape[0], <char*>&output[0], <int>output_size)
     if ret == <int>output_size + 1:
         raise ValueError("output buffer is too small")
     return ret
